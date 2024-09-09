39761e0d15aa9439e5785e16ba8d301787e25f0c
Majd833 <lina.azzam.1611@gmail.com>
2024-09-09 10:13:59 +0300

    Lesson2

---
+++ print.py
@@ -0,0 +1,8 @@
+#Writing a Sentence
+print("Hello I am using Codingal")
+#Writing Numbers
+print(4)
+#Writing Two Values Inside a Command
+print("Hello", 6)
+#ending a Sentence with a symbol
+print("Codingal", end="*") 
